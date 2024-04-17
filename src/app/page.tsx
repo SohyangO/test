9ace7bd9c077e6d1a159ed8abe81a317d452fdbd
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-17 18:10:12 +0900

    fix: 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
 import styles from './page.module.css';
 import { Image, Button, Link } from '@nextui-org/react';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const useragt = navigator.userAgent.toLowerCase();
+    const target_url = location.href;
+
+    if (useragt.match(/kakaotalk/i)) {
+      //카카오톡 외부브라우저로 호출
+      window.location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
+    } else if (useragt.match(/line/i)) {
+      if (target_url.indexOf('?') !== -1) {
+        window.location.href = target_url + '&openExternalBrowser=1';
+      } else {
+        window.location.href = target_url + '?openExternalBrowser=1';
+      }
+    }
+    if (useragt.match(/iphone|ipad|ipod/i)) {
+      // 아이폰은 강제로 사파리를 실행할 수 없다 ㅠㅠ
+      // 모바일 대응 뷰포트 강제 설정
+      const mobile = document.createElement('meta');
+      mobile.name = 'viewport';
+      mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
+      document.getElementsByTagName('head')[0].appendChild(mobile);
+    } else {
+      // 안드로이드는 Chrome이 설치되어있음으로 강제로 스킴 실행한다.
+      window.location.href =
+        'intent://' + target_url.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <section className={styles.hero}>
